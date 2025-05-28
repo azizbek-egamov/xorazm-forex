6f9074481bdccb4165797c1de02fdb5ee4f86ef8
VIctus 7 <zohid.zokirivich2000@gmail.com>
2025-05-28 22:50:07 +0500

    bular yangi qo'shildi yoki yangilandi

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Xorazm Forex',
+  description: 'Treyding akademiyasi',
+  generator: 'ardentsoft.uz',
 }
 
 export default function RootLayout({
